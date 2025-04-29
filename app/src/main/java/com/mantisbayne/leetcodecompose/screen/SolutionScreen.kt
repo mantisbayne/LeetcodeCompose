9cb0acb738b71c222dce92f3dfdd98e44db9e970
Meredith Bayne <meredithbayne@Merediths-MBP.attlocal.net>
2025-04-29 07:11:34 -0500

    screen tweaks

--- app/src/main/java/com/mantisbayne/leetcodecompose/screen/SolutionScreen.kt
+++ app/src/main/java/com/mantisbayne/leetcodecompose/screen/SolutionScreen.kt
@@ -2,6 +2,7 @@ package com.mantisbayne.leetcodecompose.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -26,6 +27,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
@@ -55,8 +57,9 @@ fun SolutionScreen(
             Text("No solutions available.")
         } else {
             LazyColumn(
-                Modifier.fillMaxWidth()
-                    .padding(8.dp)
+                Modifier.fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
                 items(solutions.filter { selectedCategory == null || it.category == selectedCategory }) { solution ->
                     SolutionItem(solution)
@@ -74,7 +77,10 @@ fun CategoryDropDown(
 ) {
     var expanded by remember { mutableStateOf(false) }
 
-    Box {
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.TopCenter
+    ) {
         Button(onClick = { expanded = true }) {
             Text(selectedCategory ?: "Select category")
         }
